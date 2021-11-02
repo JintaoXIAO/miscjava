68a994902155c3d8f50c57739bd3e433e104fd47
jintao_xiao <xiaojintao@outlook.com>
2021-11-02 17:47:57 +0800

    update

---
+++ src/main/java/algos/sort/Insertion.java
@@ -0,0 +1,11 @@
+package algos.sort;
+
+public class Insertion implements CompareBasedSortOps {
+  public <T extends Comparable<T>> void sort(T[] ts) {
+    int N = ts.length;
+    for (int i = 1; i < N; i++) {
+      for (int j = i; less(ts[j], ts[j - 1]); j--)
+        exch(ts, j, j - 1);
+    }
+  }
+}
