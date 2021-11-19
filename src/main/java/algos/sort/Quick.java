5ff8d2296360426bf3d0ac9bdb40b96909294b71
jintao_xiao <xiaojintao@outlook.com>
2021-11-19 15:33:31 +0800

    update

--- src/main/java/algos/sort/Quick.java
+++ src/main/java/algos/sort/Quick.java
@@ -1,6 +1,19 @@
 package algos.sort;
 
+import java.util.Arrays;
+
 public class Quick implements CompareBasedSort{
+
+  public static void main(String[] args) {
+    var quiz = new Character[]{'K','R','A','T','E','L','E','P','U','I','M','Q','C','X','O','S'};
+    var q = new Quick();
+    q.sort(quiz);
+
+    System.out.println(Arrays.toString(quiz));
+  }
+
+
+
   @Override
   public <T extends Comparable<T>> void sort(T[] ts) {
     sort(ts, 0, ts.length - 1);
@@ -17,25 +30,14 @@ public class Quick implements CompareBasedSort{
   <T extends Comparable<T>> int partition(T[] ts, int lo, int hi) {
     T pivot = ts[lo];
 
-    int p = lo;
     int l = 1, r = hi;
-    while (l <= r && r > 0) {
-      while (l <= r && less(ts[l], pivot)) l++;
-      while (l <= r && less(pivot, ts[r])) r--;
+    while (l < r) {
+      while (l < r && less(ts[l], pivot)) l++;
+      while (l < r && less(pivot, ts[r])) r--;
       if (l < r) exch(ts, l, r);
-      else if (l == r) {
-        if (less(ts[l], pivot)) {
-          exch(ts, l, 0);
-          p = l;
-        } else {
-          p = l - 1;
-        }
-      } else {
-        exch(ts, hi, 0);
-        p = hi;
-      }
+      else exch(ts, lo, l);
     }
-    return p;
+    return l;
   }
 
 }
