3dafaa99f70ac4721ff194017e05a7000baffc9e
jintao_xiao <xiaojintao@outlook.com>
2022-01-04 11:31:14 +0800

    leetcode :: 124 :: update

--- src/main/java/algos/misc/Lt124.java
+++ src/main/java/algos/misc/Lt124.java
@@ -6,20 +6,21 @@ public class Lt124 {
       if (root == null) return -1001;
       if (root.left == null && root.right == null) return root.val;
 
-      int m1 = root.val;
+      int m1 = root.val; // include only one root node
 
       int lm1 = maxPathSum1(root.left);
       int rm1 = maxPathSum1(root.right);
-      int m2 = m1 + lm1 + rm1;
+      int m2 = m1 + lm1 + rm1; // include root and left.path and right.path
 
       int lm = maxPathSum(root.left);
       int rm = maxPathSum(root.right);
-      int m3 = Integer.max(lm, rm);
+      int m3 = Integer.max(lm, rm); // include only left.path or right.path
 
-      int m4 = m1 + Integer.max(lm1, rm1);
+      int m4 = m1 + Integer.max(lm1, rm1); // include root and left.path or right.path
       return Integer.max(m1, Integer.max(m2, Integer.max(m3, m4)));
     }
 
+    // when include parent's node, at the current node, we can only include one path, left.path or right.path
     int maxPathSum1(TreeNode node) {
       if (node == null) return -1001;
       int m1 = node.val;
